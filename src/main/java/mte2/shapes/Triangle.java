b34684401f2162dfe9a09a1d16fc41acf3bcc9c5
Carter Hill (RIT Student) <cjh2228@rit.edu>
2026-03-04 10:49:42 -0500

    Triangle

--- src/main/java/mte2/shapes/Triangle.java
+++ src/main/java/mte2/shapes/Triangle.java
@@ -4,22 +4,28 @@
 
 package mte2.shapes;
 
-public class Triangle {
+public class Triangle implements Shape {
 // public class Triangle implements Shape {
-    // private double sideA;
-    // private double sideB;
-    // private double sideC;
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
-    // public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
+    public Triangle(double sideA,double sideB,double sideC) { this.sideA = sideA; this.sideB = sideB; this.sideC = sideC; }
 
-    // @Override
-
-    // @Override
-    // public double area() {    double p = perimeter()  / 2;    }
+    @Override
+    public double perimeter() {
+        return sideA+sideB+sideC;
+    }
+    @Override
+    public double area() {    
+        double p = perimeter()  / 2;
+        return Math.sqrt(p*(p-sideA)*(p-sideB)*(p-sideC));
+        }
 
     public static void main(String[] args) {
-        
-        // ...
+        Triangle t = new Triangle(2, 2, 2);
+        System.out.println(t.perimeter());
+        System.out.println(t.area());
 
     }
 }
