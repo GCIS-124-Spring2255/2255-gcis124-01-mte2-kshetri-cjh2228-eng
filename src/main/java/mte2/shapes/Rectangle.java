c1b67a3c0ff320cc4aad661a1e37275f028bbd8f
Carter Hill (RIT Student) <cjh2228@rit.edu>
2026-03-04 10:39:14 -0500

    rectangle

--- src/main/java/mte2/shapes/Rectangle.java
+++ src/main/java/mte2/shapes/Rectangle.java
@@ -4,20 +4,28 @@
 
 package mte2.shapes;
 
-public class Rectangle {
+public class Rectangle implements Shape {
 // public class Rectangle implements Shape {
     
-    // private final double length;
-    // private final double width;
+    private final double length;
+    private final double width;
 
-    // public Rectangle(double length,double width) {  this.length = length;  this.width = width;  }
+    public Rectangle(double length,double width) {  this.length = length;  this.width = width;  }
+    @Override
+    public double perimeter() {
+        return 2*(length*width);
+    }
+    @Override
+    public double area() {
+        return length*width;
 
-    // @Override
-    // @Override
+    }
     
     public static void main(String[] args) {
+        Rectangle r = new Rectangle(2, 2);
+        System.out.println(r.area());
+        System.out.println(r.perimeter());
         
-        // ...
 
     }
     
